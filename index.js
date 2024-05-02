fd9b9c8eeeb935cd5ff22debca5e70ed74569616
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-05-02 17:00:21 +0200

    added

--- index.js
+++ index.js
@@ -19,20 +19,6 @@ const data = [
       difficulty: "Medium",
       info: "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/"
     },
-    {
-      id: 2,
-      pic: "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/2014/03/13-flat-twist-with-twist-out-2.jpg?w=500&ssl=1",
-      name: "Flat-Twist With Twist Out",
-      difficulty: "Medium",
-      info: "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/"
-    },
-    {
-      id: 3,
-      pic: "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/2014/03/13-flat-twist-with-twist-out-2.jpg?w=500&ssl=1",
-      name: "Flat-Twist With Twist Out",
-      difficulty: "Medium",
-      info: "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/"
-    }
   ];
   
   const buildHairstyleCard = (hairstyle) => {
