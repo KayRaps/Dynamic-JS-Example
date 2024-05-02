5b4169917a46edc8e58cadb8e8e1b48ef7f74b75
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-05-02 16:58:40 +0200

    commit

--- index.js
+++ index.js
@@ -8,3 +8,52 @@ submitButton.addEventListener('click', function(){
         greeting.textContent = `Hello, ${name}!`;
     }
 });
+
+
+
+const data = [
+    {
+      id: 1,
+      pic: "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/2014/03/13-flat-twist-with-twist-out-2.jpg?w=500&ssl=1",
+      name: "Flat-Twist With Twist Out",
+      difficulty: "Medium",
+      info: "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/"
+    },
+    {
+      id: 2,
+      pic: "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/2014/03/13-flat-twist-with-twist-out-2.jpg?w=500&ssl=1",
+      name: "Flat-Twist With Twist Out",
+      difficulty: "Medium",
+      info: "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/"
+    },
+    {
+      id: 3,
+      pic: "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/2014/03/13-flat-twist-with-twist-out-2.jpg?w=500&ssl=1",
+      name: "Flat-Twist With Twist Out",
+      difficulty: "Medium",
+      info: "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/"
+    }
+  ];
+  
+  const buildHairstyleCard = (hairstyle) => {
+    // Create elements needed to build a card
+    const div = document.createElement('div')
+    const h3 = document.createElement('h3')
+    const a = document.createElement('a')
+    const img = document.createElement('img')
+  
+    // Append newly created elements into the DOM
+    const body = document.querySelector('body');
+    body.append(div)
+    h3.append(a)
+    div.append(h3)
+    div.append(img)
+  
+    // Set content and attributes
+    a.innerHTML = hairstyle.name
+    a.setAttribute('href', hairstyle.info)
+    img.setAttribute('src', hairstyle.pic)
+    div.setAttribute('class', 'card')
+  }
+  
+  data.forEach(hairstyle => buildHairstyleCard(hairstyle))
